a80f36c210cc764a784c6535eac218ef9a864fe5
zyb <1140114702@qq.com>
2017-03-29 18:07:51 +0800

    修改无限轮播

--- app/src/main/java/com/example/bin/bannerdemo/MainActivity.java
+++ app/src/main/java/com/example/bin/bannerdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bin.bannerdemo;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     private List<ImageView> pointList;
+    private int pointCurrentItem;
+    private boolean startBanner = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +30,10 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     }
 
     private void initView() {
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         LinearLayout pointLayout = (LinearLayout) findViewById(R.id.point_layout);
 
-        List<View> views = new ArrayList<>();
+        List<TextView> views = new ArrayList<>();
         TextView textView;
         for (int i = 0; i < 3; i++) {
             textView = new TextView(this);
@@ -41,6 +45,22 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
         viewPager.setAdapter(new MyAdapter(views));
         viewPager.addOnPageChangeListener(this);
+        viewPager.setCurrentItem(0);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (startBanner) {
+                    SystemClock.sleep(2000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                        }
+                    });
+                }
+            }
+        }).start();
 
         //设置轮播的点
         pointList = new ArrayList<>();
@@ -50,15 +70,17 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 //            layoutParams.rightMargin = 12; //设置边距
 //            layoutParams.leftMargin = 12; //设置边距
 //            point.setLayoutParams(layoutParams);
-            point.setPadding(5,0,5,0);
-            if (i == 0) {
-                point.setImageResource(R.drawable.banner_point_select);
-            } else {
-                point.setImageResource(R.drawable.banner_point_no_select);
-            }
+            point.setPadding(5, 0, 5, 0);
+//            if (i == 0) {
+//                point.setImageResource(R.drawable.banner_point_select);
+//            } else {
+//                point.setImageResource(R.drawable.banner_point_no_select);
+//            }
+            point.setImageResource(R.drawable.banner_point_no_select);
             pointLayout.addView(point);
             pointList.add(point);
         }
+        pointList.get(pointCurrentItem).setImageResource(R.drawable.banner_point_select);
     }
 
     @Override
@@ -67,10 +89,11 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     @Override
     public void onPageSelected(int position) {
-        for (int i = 0; i < pointList.size(); i++) {
-            pointList.get(i).setImageResource(R.drawable.banner_point_no_select);
-        }
-        pointList.get(position).setImageResource(R.drawable.banner_point_select);
+        Log.e("position", "onPageSelected: " + position);
+        int newPoint = position % pointList.size();
+        pointList.get(pointCurrentItem).setImageResource(R.drawable.banner_point_no_select);
+        pointList.get(newPoint).setImageResource(R.drawable.banner_point_select);
+        pointCurrentItem = newPoint;
     }
 
     @Override
@@ -79,15 +102,15 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     private class MyAdapter extends PagerAdapter {
 
-        private List<View> list;
+        private List<TextView> list;
 
-        MyAdapter(List<View> list) {
+        MyAdapter(List<TextView> list) {
             this.list = list;
         }
 
         @Override
         public int getCount() {
-            return list.size();
+            return Integer.MAX_VALUE;
         }
 
         @Override
@@ -97,13 +120,26 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            container.addView(list.get(position));
-            return list.get(position);
+
+          container.addView(list.get(position%list.size()));
+            return list.get(position % list.size());
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView(list.get(position));
+            container.removeView(list.get(position % list.size()));
         }
+    }
+
+    @Override
+    protected void onPause() {
+        startBanner = false;
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        startBanner = true;
+        super.onResume();
     }
 }
